768032bd1fe059f36279862cf57e2637487ef32a
Billy <cryptobilly37@gmail.com>
2023-10-06 10:40:04 -0600

    Add script

---
+++ src/script.ts
@@ -0,0 +1,30 @@
+import { config } from 'dotenv-flow';
+import fetch, { Headers } from 'node-fetch';
+import { sendRaffleTweet } from './tweet.js';
+
+config();
+
+const go = async () => {
+  const slug = 'forge-10x-wl-nzgfqo';
+  const raffleResp = await fetch(
+    `https://api.alphabot.app/v1/raffles/${slug}`,
+    {
+      headers: new Headers({
+        authorization: `Bearer ${process.env.ALPHABOT_API_KEY}`,
+      }),
+    }
+  );
+
+  const json = (await raffleResp.json()) as any;
+
+  if (!json?.data?.raffle) {
+    console.log('INVALID RAFFLE RESPONSE', json);
+    return;
+  }
+
+  console.log('TWEETING', json.data.raffle);
+
+  await sendRaffleTweet(json.data.raffle);
+};
+
+go().then(() => process.exit(0));
